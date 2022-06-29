5ba6a40e7afb9772015c5670c502478257ac28bd
ronald <gahima.ronald@yahoo.com>
2022-06-29 20:26:16 +0000

    correction

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for c in range(122, 96, -1):
-    if (c % 2 != 0):
-        c -= 32
-    print("{}".format(chr(c)), end="")
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        print("{:c}".format(i - 32), end='')
+    else:
+        print("{:c}".format(i), end='')
