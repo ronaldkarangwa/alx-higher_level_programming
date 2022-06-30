baa8e84afefba86d678a9f3ea0184bfc6cd7efe9
karangwa <gahima.ronald@yahoo.com>
2022-06-30 10:45:29 -0700

    corrections

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
