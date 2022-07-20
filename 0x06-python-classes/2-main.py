ddda386747ba1af69bc6d729cb3c7718312b5601
ronald <gahima.ronald@yahoo.com>
2022-07-20 20:46:09 +0000

    correction

--- 0x06-python-classes/2-main.py
+++ 0x06-python-classes/2-main.py
@@ -32,13 +32,3 @@ try:
     print(my_square_4.__dict__)
 except Exception as e:
     print(e)
-
-guillaume@ubuntu:~/0x06$ ./2-main.py
-<class '2-square.Square'>
-{'_Square__size': 3}
-<class '2-square.Square'>
-{'_Square__size': 0}
-'Square' object has no attribute 'size'
-'Square' object has no attribute '__size'
-size must be an integer
-size must be >= 0
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""defines a class Square"""
+class Square():
+    '''
+        Defining a Square
+    '''
 
-
-class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
     def __init__(self, size=0):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-        Returns:
-            None
-        """
-        if type(size) is not int:
+        '''Initialization of instance attributes
+            Args:
+            size (int): Zero or positve number.
+        '''
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = 
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
