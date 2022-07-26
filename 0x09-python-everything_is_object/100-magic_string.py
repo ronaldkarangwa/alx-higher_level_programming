83c91621a30e292e43a38ce2ab7c411f82a69e3c
karangwa <gahima.ronald@yahoo.com>
2022-07-26 12:06:46 -0700

    advanced task

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append("Holberton")
-    return ", ".join(l)
+def magic_string(static={"count": 0}):
+    static["count"] += 1
+    return str("BestSchool, " * static["count"])[:-2]
